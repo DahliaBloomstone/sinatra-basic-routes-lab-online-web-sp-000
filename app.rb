f5a23c522de42bd01301f417802abb176731b34f
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-25 21:48:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,9 +4,17 @@ class App < Sinatra::Base
 
 get '/name' do
   "200 status code"
+  My name is name
 end
 
+get '/hometown' do
+  "200 status code"
+  My hometown is
+end
 
-
+get '/favorite-song' do
+  "200 status code"
+  My favorite song is 
+end
 
 end
